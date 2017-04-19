b64423586e25622811801f4376077a06e88bc4c5
derek-yi <derek_yi@163.com>
2017-04-19 20:21:00 +0800

    abc

--- time1.py
+++ time1.py
@@ -6,11 +6,11 @@ import datetime
 #计算今天的时间
 today = datetime.date.today()
 
-# 计算昨天的时间  
-yesterday = today - datetime.timedelta(days = 1)
-
 # test
 tomorrow = today + datetime.timedelta(days = 1)  
+
+# test
+yesterday = today - datetime.timedelta(days = 1)
 
 # 打印这三个时间
 print(yesterday, today, tomorrow)
